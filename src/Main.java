78f5d0d3a643ce9906cd2660cbaacf8a3754d0b1
KsuChe <nerro@mail.ru>
2022-05-12 21:59:41 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,34 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static void isYearLeap(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println(year + " год является високосным");
-        }else {
-            System.out.println(year + " год не является високосным");
-        }
+
+
+//    public static void task1 (isYearLeap(2020)) {
+//        public static void task1(); {
+//            isYearLeap(2020);
+//            public static void isYearLeap(int year) {
+//                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+//                    System.out.println(year + " год является високосным");
+//                }else {
+//                    System.out.println(year + " год не является високосным");
+//                }
+//            }}
+//
+//    }
+
+
+
+    public static void main (int clientOS, int currentYear) {
+        checkDevice(0,2022);
     }
-    public static void main(String[] args) {
-        isYearLeap(2021);
+    public static void checkDevice(int clientOS, int currentYear) {
+        if (clientOS == 0 && currentYear == LocalDate.now().getYear()) {
+            System.out.println("Установите мобильную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && currentYear < LocalDate.now().getYear()) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && currentYear >= LocalDate.now().getYear()) {
+            System.out.println("Установите мобильную версию приложения для Android по ссылке");
+        }else System.out.println("Установите облегченную версию приложения для Android по ссылке");
     }
 
-    }
-
+}
